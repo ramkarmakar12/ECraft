bbe4b4f72a509947313d19e366f8f079c99a4f39
Ramkrishna Karmakar <remoramu12@Gmail.com>
2022-11-05 18:47:58 +0530

    added images, data and ui

---
+++ frontend/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: 'Handmade lamp',
+      slug: 'handmade-lamp',
+      category: 'lamp',
+      image: '/images/indoor-lamp.jpg',
+      price: 100,
+      countInStock: 10,
+      brand: 'local',
+      rating: 4.5,
+      numReviews: 1,
+      descriptin: 'High quality indoor lamp',
+    },
+
+    {
+      name: 'Homdecor',
+      slug: 'handmade-decor',
+      category: 'decoration',
+      image: '/images/home-decor.jpg',
+      price: '200',
+      countInStock: '10',
+      brand: 'local',
+      rating: 4.5,
+      numReviews: 1,
+      descriptin: 'High quality decoration item',
+    },
+
+    {
+      name: 'Handmade teacup',
+      slug: 'handmade-teacup',
+      category: 'kitchen',
+      image: '/images/tea-cup.jpg',
+      price: 120,
+      countInStock: 15,
+      brand: 'local',
+      rating: 4.5,
+      numReviews: 10,
+      descriptin: 'High quality water jug',
+    },
+
+    {
+      name: 'Handmade bag',
+      slug: 'handmade-bag',
+      category: 'bag',
+      image: '/images/printed-bag.jpg',
+      price: 300,
+      countInStock: 25,
+      brand: 'local',
+      rating: 4.5,
+      numReviews: 12,
+      descriptin: 'High quality handbag',
+    },
+  ],
+};
+
+export default data;
