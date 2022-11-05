1558cb7221d35f507235a61f771b659ad96b05df
Ramkrishna Karmakar <remoramu12@Gmail.com>
2022-11-05 18:03:01 +0530

    Create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">ECraft</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
